66105068c7292b3985bdec7e93aa63abf0bfeba1
Daniel Da Rocha <daniel@itden.net>
2022-03-30 17:19:49 -0400

    delete post route add. New adjustments to routing and Home page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const _ = require('lodash');
 const array = require('lodash/array');
 const object = require('lodash/fp/object');
 const { trimEnd } = require("lodash");
+const cookieParser = require('cookie-parser')
 
 
 const app = express();
@@ -16,17 +17,29 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(cookieParser())
+
 
 
 // Global Variables
 
 var posts = [];
 
-
 // ---- Get Routes
+
 app.get("/", function(req,res){
-  res.render('home',{posts: posts});
-  console.log("Someone accessed the home page from -> "+req.ip);
+  let user = 'user'+Math.floor(Math.random() * 999999999);
+  
+
+  if (req.cookies.name === undefined) {
+    res.cookie('name', user)
+    res.render('home',{posts: posts});
+  } else {
+    res.render('home',{posts: posts, cookieName: req.cookies.name});
+  }
+
+  console.log(req.cookies.name + " accessed the home page from -> "+ req.ip);
+
 });
 
 app.get("/posts/:postID", function(req,res){
@@ -52,13 +65,28 @@ app.get("/why", function(req,res){
   res.render('why',{});
 });
 
+
+
 // ---- Post Routes
+
+app.post("/delete", function(req,res){
+
+  for (var i = posts.length - 1; i >= 0; --i) {
+    if (posts[i].userId == req.cookies.name && posts[i].postTitle == req.body.postBack) {
+        posts.splice(i,1);
+    }
+}
+  console.log(req.cookies.name+ " just deleted a post with the name: " + req.body.postBack);
+  res.redirect("/");
+
+});
 
 app.post('/vent', function(req,res){
 
 const post = {
 postTitle: req.body.title, 
-postBody: req.body.content
+postBody: req.body.content,
+userId: req.cookies.name
 };
 
 posts.push(post);
